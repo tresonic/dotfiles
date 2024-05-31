64892b831df88484dfd041230f472c47049da700
tresonic <tresonic@mail.de>
2024-05-31 22:59:43 +0200

    use yay -Qeq to dump pkglist

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-# install packages
-sudo pacman -S -needed - < pkglist.txt
-
 # install yay
 git clone https://aur.archlinux.org/yay-bin.git && cd yay-bin && makepkg -si && cd .. && rm -rf yay-bin
+
+# install packages
+sudo yay -S -needed - < pkglist.txt
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
