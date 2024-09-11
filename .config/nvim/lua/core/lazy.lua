3f43dc081c2801f751584ea0147a5d118ac6f953
tresonic <37078118+tresonic@users.noreply.github.com>
2024-09-11 22:45:23 +0200

    add nvim config

---
+++ .config/nvim/lua/core/lazy.lua
@@ -0,0 +1,128 @@
+-----------------------------------------------------------
+-- Plugin manager configuration file
+-----------------------------------------------------------
+
+-- Plugin manager: lazy.nvim
+-- URL: https://github.com/folke/lazy.nvim
+
+-- For information about installed plugins see the README:
+-- neovim-lua/README.md
+-- https://github.com/brainfucksec/neovim-lua#readme
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Use a protected call so we don't error out on first use
+local status_ok, lazy = pcall(require, 'lazy')
+if not status_ok then
+  return
+end
+
+-- Start setup
+lazy.setup({
+  spec = {
+    -- Colorscheme:
+    -- The colorscheme should be available when starting Neovim.
+    {
+      'navarasu/onedark.nvim',
+      lazy = false, -- make sure we load this during startup if it is your main colorscheme
+      priority = 1000, -- make sure to load this before all the other start plugins
+    },
+
+    -- Icons
+    { 'kyazdani42/nvim-web-devicons', lazy = true },
+
+    -- Git labels
+    {
+      'lewis6991/gitsigns.nvim',
+      lazy = true,
+      dependencies = {
+        'nvim-lua/plenary.nvim',
+        'kyazdani42/nvim-web-devicons',
+      },
+      config = function()
+        require('gitsigns').setup{}
+      end
+    },
+
+    -- File explorer
+    {
+      'kyazdani42/nvim-tree.lua',
+      dependencies = { 'kyazdani42/nvim-web-devicons' },
+    },
+
+    -- Statusline
+    {
+      'freddiehaddad/feline.nvim',
+      dependencies = {
+        'kyazdani42/nvim-web-devicons',
+        'lewis6991/gitsigns.nvim',
+      },
+    },
+
+    -- Treesitter
+    { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+
+    -- Indent line
+    { 'lukas-reineke/indent-blankline.nvim', main = 'ibl', opts = {} },
+
+    -- Autopair
+    {
+      'windwp/nvim-autopairs',
+      event = 'InsertEnter',
+      config = function()
+        require('nvim-autopairs').setup{}
+      end
+    },
+
+    -- LSP
+    { 'neovim/nvim-lspconfig' },
+
+    -- Autocomplete
+    {
+      'hrsh7th/nvim-cmp',
+      -- load cmp on InsertEnter
+      event = 'InsertEnter',
+      -- these dependencies will only be loaded when cmp loads
+      -- dependencies are always lazy-loaded unless specified otherwise
+      dependencies = {
+        'L3MON4D3/LuaSnip',
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-path',
+        'hrsh7th/cmp-buffer',
+        'saadparwaiz1/cmp_luasnip',
+      },
+    },
+
+    -- Jump to files fast
+    {
+      'nvim-telescope/telescope.nvim',
+      tag = '0.1.8',
+      dependencies = { 'nvim-lua/plenary.nvim' },
+    },
+
+    -- Commenting
+    { 'numToStr/Comment.nvim' },
+
+    -- Show buffers
+    {
+      'akinsho/bufferline.nvim',
+      version = "v4.7.0",
+      dependencies = 'nvim-tree/nvim-web-devicons',
+      config = function()
+        require('bufferline').setup{}
+      end
+    },
+  },
+})
