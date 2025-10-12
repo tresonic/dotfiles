cf976586029bcb14fe12576fe6a105f94fc726a8
tresonic <tresonic@mail.de>
2025-10-12 08:49:00 +0200

    nvim: update lsp config for lspconfig 0.11

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -15,16 +15,16 @@ return {
               "--query-driver=**"
             },
         },
-        -- zls = {},
+        -- zls = {}, -- managed by zig-lamp
       }
     },
     config = function(_, opts)
-      local lspconfig = require('lspconfig')
       for server, config in pairs(opts.servers) do
         -- passing config.capabilities to blink.cmp merges with the capabilities in your
         -- `opts[server].capabilities, if you've defined it
         config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
-        lspconfig[server].setup(config)
+        vim.lsp.enable(server)
+        vim.lsp.config(server, config)
       end
     end
   },
